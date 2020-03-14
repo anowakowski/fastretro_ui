6efd78bf600a86769345850cf10ed96f8393fff4
anowakowski <anowakowski@mailc.net>
2020-03-14 09:56:09 +0100

    use example for navbar

--- retroDrive/src/app/modules/login-register/login-register.module.ts
+++ retroDrive/src/app/modules/login-register/login-register.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginRegisterComponent } from './login-register.component';
 import { LoginRegisterRoutingModule } from './login-register-routing.module';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   imports: [
     CommonModule,
-    LoginRegisterRoutingModule
+    LoginRegisterRoutingModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   declarations: [LoginRegisterComponent]
 })
