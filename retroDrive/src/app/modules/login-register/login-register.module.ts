a67429fd36114de0847183a631d4958a04963b9f
anowakowski <anowakowski@mailc.net>
2020-03-14 08:46:33 +0100

    added login-register module

---
+++ retroDrive/src/app/modules/login-register/login-register.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginRegisterComponent } from './login-register.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [LoginRegisterComponent]
+})
+export class LoginRegisterModule { }
