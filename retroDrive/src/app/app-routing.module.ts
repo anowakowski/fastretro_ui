b7248f2dde26cd6c5054d9213038e543f6018306
anowakowski <anowakowski@mailc.net>
2020-03-14 08:48:50 +0100

    register modules

--- retroDrive/src/app/app-routing.module.ts
+++ retroDrive/src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'login',
+  loadChildren: '../app/modules/login-register/login-register.module#LoginRegisterModule'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
