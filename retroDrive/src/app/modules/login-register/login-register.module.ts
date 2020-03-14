2607e6647b4ef8121d730b327c4d12c533287104
anowakowski <anowakowski@mailc.net>
2020-03-14 10:18:44 +0100

    next part of login-register elements

--- retroDrive/src/app/modules/login-register/login-register.module.ts
+++ retroDrive/src/app/modules/login-register/login-register.module.ts
@@ -1,17 +1,30 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { LoginRegisterComponent } from './login-register.component';
 import { LoginRegisterRoutingModule } from './login-register-routing.module';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     LoginRegisterRoutingModule,
+    FlexLayoutModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatCardModule
   ],
-  declarations: [LoginRegisterComponent]
+  declarations: [LoginRegisterComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class LoginRegisterModule { }
