d4a31a7fadcaa82dafef8b44c0d7e43b89ee7c7f
anowakowski <anowakowski@mailc.net>
2020-03-14 18:13:06 +0100

    prepare children routing

--- retroDrive/src/app/modules/team-retro/team-retro-routing.module.ts
+++ retroDrive/src/app/modules/team-retro/team-retro-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TeamRetroComponent } from './team-retro.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
 
 const routes: Routes = [
   {
     path: '',
-    component: TeamRetroComponent
+    component: TeamRetroComponent,
+    children: [
+      {path: '', component: DashboardComponent},
+    ]
   }
 ];
 
