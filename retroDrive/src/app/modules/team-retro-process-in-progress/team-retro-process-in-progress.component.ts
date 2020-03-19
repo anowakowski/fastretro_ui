970e3466ec747e78d37cd189188f887eabbc70d6
anowakowski <anowakowski@mailc.net>
2020-03-19 19:44:48 +0100

    added module for retro processing

---
+++ retroDrive/src/app/modules/team-retro-process-in-progress/team-retro-process-in-progress.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-team-retro-process-in-progress',
+  templateUrl: './team-retro-process-in-progress.component.html',
+  styleUrls: ['./team-retro-process-in-progress.component.css']
+})
+export class TeamRetroProcessInProgressComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ retroDrive/src/app/modules/team-retro-process-in-progress/team-retro-process-in-progress.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TeamRetroProcessInProgressComponent } from './team-retro-process-in-progress.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [TeamRetroProcessInProgressComponent]
+})
+export class TeamRetroProcessInProgressModule { }
