eb5cf638db2df38ec8b4600102cab939d502864d
anowakowski <anowakowski@mailc.net>
2020-03-20 13:49:11 +0100

    prepare first part with timer functionality

--- retroDrive/src/app/modules/team-retro-process-in-progress/team-retro-process-in-progress.component.ts
+++ retroDrive/src/app/modules/team-retro-process-in-progress/team-retro-process-in-progress.component.ts
@@ -1,25 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { Observable, interval } from 'rxjs';
 
 @Component({
   selector: 'app-team-retro-process-in-progress',
   templateUrl: './team-retro-process-in-progress.component.html',
   styleUrls: ['./team-retro-process-in-progress.component.css']
 })
-export class TeamRetroProcessInProgressComponent implements OnInit {
+export class TeamRetroProcessInProgressComponent implements OnInit, OnDestroy {
 
+  private timerSubscription: any;
+  private counter: Observable<number>;
   current = 5;
   max = 10;
 
   constructor() { }
 
   ngOnInit() {
+    this.setCounter();
   }
 
-  doSomethingWithCurrentValue(progressBarValue){
-    const me = this;
-    if (progressBarValue === me.max) {
-      //me.ngOnDestroy();
+  ngOnDestroy() {
+    this.unsubscribeTimer();
+  }
+
+  doSomethingWithCurrentValue(progressBarValue) {
+    if (progressBarValue === this.max) {
+      this.unsubscribeTimer();
     }
+  }
+
+
+
+  unsubscribeTimer () {
+    this.timerSubscription.unsubscribe();
+  }
+
+  subscribeTimer() {
+    this.timerSubscription = this.counter.subscribe(sec => {
+      this.current++;
+    });
+  }
+
+  setCounter(){
+    const me = this;
+    me.counter = interval(1000);
   }
 
 
