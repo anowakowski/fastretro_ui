e79e381661fe3e401dc286fe57288cbb011dce02
anowakowski <anowakowski@mailc.net>
2020-03-25 22:42:12 +0100

    added new timer option functionality in event services

--- retroDrive/src/app/services/events.service.ts
+++ retroDrive/src/app/services/events.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, EventEmitter } from '@angular/core';
+import { TimerOption } from '../models/timerOption';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +9,7 @@ export class EventsService {
 constructor() { }
 
   private stopRetroInProgressProcessEmiter: EventEmitter<boolean> = new EventEmitter();
+  private timerOptionsEmiter: EventEmitter<TimerOption> = new EventEmitter();
 
   emitStopRetroInProgressProcessEmiter(shouldStopRetroProcess) {
     this.stopRetroInProgressProcessEmiter.emit(shouldStopRetroProcess);
@@ -17,6 +19,12 @@ constructor() { }
     return this.stopRetroInProgressProcessEmiter;
   }
 
+  emitTimerOptions(timerOption: TimerOption) {
+    this.timerOptionsEmiter.emit(timerOption);
+  }
 
+  getTimerOptionsEmiter() {
+    return this.timerOptionsEmiter;
+  }
 }
 
