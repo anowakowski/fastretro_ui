20bd362d820eadec496837d984a2eda7329a19de
anowakowski <anowakowski@mailc.net>
2020-03-25 11:23:29 +0100

    change retro in progress when retro is finised

--- retroDrive/src/app/modules/team-retro-process-in-progress/components/nav/nav.component.ts
+++ retroDrive/src/app/modules/team-retro-process-in-progress/components/nav/nav.component.ts
@@ -1,21 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { EventsService } from 'src/app/services/events.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent implements OnInit {
+export class NavComponent implements OnInit, OnDestroy {
 
-  public shouldStopTimer = false;
+  public shouldChangeRetroDisplayText = false;
+  public stopRetroInProgressProcessSubscriptions: any;
 
-  constructor() { }
+  constructor(private eventsServices: EventsService) { }
 
   ngOnInit() {
+    this.subscribeEvents();
+  }
+
+  ngOnDestroy() {
+    this.stopRetroInProgressProcessSubscriptions.unsubscribe();
   }
 
   stopTimer() {
-    this.shouldStopTimer = true;
+    this.shouldChangeRetroDisplayText = true;
   }
 
+  private subscribeEvents() {
+    this.stopRetroInProgressProcessSubscriptions =
+      this.eventsServices.getStopRetroInProgressProcessEmiter().subscribe(shouldStopRetroProcess => {
+      if (shouldStopRetroProcess) {
+        this.shouldChangeRetroDisplayText = true;
+      }
+    });
+  }
 }
