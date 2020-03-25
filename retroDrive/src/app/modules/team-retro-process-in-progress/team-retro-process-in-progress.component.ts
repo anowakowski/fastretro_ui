7db70c78148af59e1a673592a9217ae954b74b3c
anowakowski <anowakowski@mailc.net>
2020-03-25 12:08:02 +0100

    prepare functionality to get data from retro board data

--- retroDrive/src/app/modules/team-retro-process-in-progress/team-retro-process-in-progress.component.ts
+++ retroDrive/src/app/modules/team-retro-process-in-progress/team-retro-process-in-progress.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-team-retro-process-in-progress',
   templateUrl: './team-retro-process-in-progress.component.html',
   styleUrls: ['./team-retro-process-in-progress.component.css']
 })
-export class TeamRetroProcessInProgressComponent implements OnInit, OnDestroy {
+export class TeamRetroProcessInProgressComponent implements OnInit {
 
-  constructor() { }
+  data: any;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
-
-  }
-
-  ngOnDestroy() {
+    if (this.route.snapshot.data['retroBoardData']) {
+      this.data = this.route.snapshot.data['retroBoardData'];
+      console.log(this.data);
+    }
   }
 }
