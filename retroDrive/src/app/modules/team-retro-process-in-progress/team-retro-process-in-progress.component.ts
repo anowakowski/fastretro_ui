6aa1977a0497fde09c928fb99baa34b3915f24f7
anowakowski <anowakowski@mailc.net>
2020-03-27 11:57:06 +0100

    added spinner to retro process

--- retroDrive/src/app/modules/team-retro-process-in-progress/team-retro-process-in-progress.component.ts
+++ retroDrive/src/app/modules/team-retro-process-in-progress/team-retro-process-in-progress.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { SpinnerTickService } from 'src/app/services/spinner-tick.service';
 
 @Component({
   selector: 'app-team-retro-process-in-progress',
   templateUrl: './team-retro-process-in-progress.component.html',
   styleUrls: ['./team-retro-process-in-progress.component.css']
 })
-export class TeamRetroProcessInProgressComponent implements OnInit {
+export class TeamRetroProcessInProgressComponent implements OnInit, OnDestroy {
 
   data: any;
   shouldShowContent = false;
-  constructor(private route: ActivatedRoute, private spinner: NgxSpinnerService) { }
+  private spinnerTickSubscription: any;
+  constructor(private route: ActivatedRoute, private spinner: NgxSpinnerService, private spinnerTickService: SpinnerTickService) { }
 
   ngOnInit() {
     if (this.route.snapshot.data['retroBoardData']) {
@@ -22,11 +24,23 @@ export class TeamRetroProcessInProgressComponent implements OnInit {
     this.spinnerTick();
   }
 
+  ngOnDestroy(): void {
+    this.unsubscribeTickService();
+  }
+
+  private unsubscribeTickService() {
+    this.spinnerTickSubscription.unsubscribe();
+  }
+
   private spinnerTick() {
     this.spinner.show();
-    setTimeout(() => {
-      this.spinner.hide();
-      this.shouldShowContent = true;
-    }, 2000);
+    this.spinnerTickSubscription = this.spinnerTickService.runNewTimer(1000).subscribe((interval) => {
+      if (interval === 1) {
+        this.shouldShowContent = true;
+      } else if (interval === 2) {
+        this.spinner.hide();
+        this.unsubscribeTickService();
+      }
+    });
   }
 }
