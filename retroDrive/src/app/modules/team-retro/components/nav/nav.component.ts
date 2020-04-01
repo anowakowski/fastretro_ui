9695792d2b717ba92b94ea8b26557c6336fdc64e
anowakowski <anowakowski@mailc.net>
2020-04-01 17:39:11 +0200

    added workspace name in nav

--- retroDrive/src/app/modules/team-retro/components/nav/nav.component.ts
+++ retroDrive/src/app/modules/team-retro/components/nav/nav.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
 import { LocalStorageService } from 'src/app/services/local-storage.service';
 import { User } from 'src/app/models/user';
+import { UserWorkspace } from 'src/app/models/userWorkspace';
 
 @Component({
   selector: 'app-nav',
@@ -14,11 +15,15 @@ export class NavComponent implements OnInit {
   constructor(public auth: AuthService, private router: Router, private localStorageService: LocalStorageService) { }
 
   currentUser: User;
+  public userWorkspace: UserWorkspace;
+  public currentUserWorkspaceName: string;
 
   @Output() toggleSidenav = new EventEmitter<void>();
 
   ngOnInit() {
     this.currentUser = this.localStorageService.getItem('currentUser');
+    this.userWorkspace = this.localStorageService.getItem('userWorkspace');
+    this.currentUserWorkspaceName = this.userWorkspace.workspaces[0].name;
   }
 
   signOut() {
