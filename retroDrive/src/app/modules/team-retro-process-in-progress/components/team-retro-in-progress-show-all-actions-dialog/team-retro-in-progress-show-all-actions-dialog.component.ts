1808eaca87b27b00b53eb703cfc32780ad69db89
anowakowski <anowakowski@mailc.net>
2020-04-18 21:58:36 +0200

    fixed base issues in new dialog

--- retroDrive/src/app/modules/team-retro-process-in-progress/components/team-retro-in-progress-show-all-actions-dialog/team-retro-in-progress-show-all-actions-dialog.component.ts
+++ retroDrive/src/app/modules/team-retro-process-in-progress/components/team-retro-in-progress-show-all-actions-dialog/team-retro-in-progress-show-all-actions-dialog.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { RetroBoardCard } from 'src/app/models/retroBoardCard';
+import { RetroBoardCardActions } from 'src/app/models/retroBoardCardActions';
 
 @Component({
   selector: 'app-team-retro-in-progress-show-all-actions-dialog',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamRetroInProgressShowAllActionsDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<TeamRetroInProgressShowAllActionsDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public dataRetroBoardCards: RetroBoardCard[]
+  ) { }
 
   ngOnInit() {
+    this.prepareActions();
   }
 
+  public prepareActions() {
+    this.dataRetroBoardCards.forEach(retroBoardCard => {
+      const actions = new Array<RetroBoardCardActions>();
+      retroBoardCard.actions.forEach(action => {
+        action.get().then(actionSnapshot => {
+          const retroBoardCardAction = actionSnapshot.data() as RetroBoardCardActions;
+          const docId = actionSnapshot.id;
+          retroBoardCardAction.id = docId;
+          actions.push(retroBoardCardAction);
+        });
+      });
+      retroBoardCard.actions = actions;
+    });
+  }
 }
