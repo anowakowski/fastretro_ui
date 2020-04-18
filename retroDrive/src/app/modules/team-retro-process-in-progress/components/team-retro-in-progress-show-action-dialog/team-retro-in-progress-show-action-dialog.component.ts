c40f67bdd0391a1314a87130b9ddbf636aabff4a
anowakowski <anowakowski@mailc.net>
2020-04-18 15:23:11 +0200

    added first part of action style

--- retroDrive/src/app/modules/team-retro-process-in-progress/components/team-retro-in-progress-show-action-dialog/team-retro-in-progress-show-action-dialog.component.ts
+++ retroDrive/src/app/modules/team-retro-process-in-progress/components/team-retro-in-progress-show-action-dialog/team-retro-in-progress-show-action-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { RetroBoardCard } from 'src/app/models/retroBoardCard';
+import { FiresrtoreRetroProcessInProgressService } from '../../services/firesrtore-retro-process-in-progress.service';
 
 @Component({
   selector: 'app-team-retro-in-progress-show-action-dialog',
@@ -11,11 +12,27 @@ export class TeamRetroInProgressShowActionDialogComponent implements OnInit {
 
   constructor(
     public dialogRef: MatDialogRef<TeamRetroInProgressShowActionDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public dataRetroBoardCard: RetroBoardCard) { }
+    @Inject(MAT_DIALOG_DATA) public dataRetroBoardCard: RetroBoardCard,
+    private firestoreService: FiresrtoreRetroProcessInProgressService) { }
+
+  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
+  actions: any[];
 
   ngOnInit() {
-    console.log(this.dataRetroBoardCard);
+    this.prepareActions();
   }
 
 
+
+  private prepareActions() {
+    this.actions = new Array<any>();
+    this.dataRetroBoardCard.actions.forEach(action => {
+      action.get().then(actionSnapshot => {
+        const actionText = actionSnapshot.data().text;
+        const actionId = actionSnapshot.id;
+        // tslint:disable-next-line:object-literal-shorthand
+        this.actions.push({ actionText: actionText, actionId: actionId });
+      });
+    });
+  }
 }
