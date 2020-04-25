d05844d294c47b6ae7235facfd487b7d7aea42cc
Adrian Nowakowski <anowakowski@mailc.net>
2020-04-25 10:18:36 +0200

    add dialog to join to existing team

---
+++ retroDrive/src/app/modules/team-retro/components/join-to-existing-team-dialog/join-to-existing-team-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-join-to-existing-team-dialog',
+  templateUrl: './join-to-existing-team-dialog.component.html',
+  styleUrls: ['./join-to-existing-team-dialog.component.css']
+})
+export class JoinToExistingTeamDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
