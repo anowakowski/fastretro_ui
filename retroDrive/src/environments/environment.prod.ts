11b5563bfaff8f079e36a102b901ac461c42d2a2
Adrian Nowakowski <anowakowski@mailc.net>
2020-04-29 12:01:37 +0200

    add firebase config to prod env

--- retroDrive/src/environments/environment.prod.ts
+++ retroDrive/src/environments/environment.prod.ts
@@ -1,6 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-
+    apiKey: 'AIzaSyB4lCzYIdFJerDY8F6VVjUbpIHr0wOe46s',
+    authDomain: 'retrodrive-7b341.firebaseapp.com',
+    databaseURL: 'https://retrodrive-7b341.firebaseio.com',
+    projectId: 'retrodrive-7b341',
+    storageBucket: 'retrodrive-7b341.appspot.com',
+    messagingSenderId: '389979442853',
+    appId: '1:389979442853:web:b1ab2fdc11e84660d0874c'
   }
 };
