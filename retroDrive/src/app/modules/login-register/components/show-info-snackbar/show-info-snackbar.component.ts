fab619661ef14313b910040ce4840f73de47ef0a
Adrian Nowakowski <anowakowski@mailc.net>
2020-05-03 12:19:36 +0200

    added info snackbar

---
+++ retroDrive/src/app/modules/login-register/components/show-info-snackbar/show-info-snackbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-info-snackbar',
+  templateUrl: './show-info-snackbar.component.html',
+  styleUrls: ['./show-info-snackbar.component.css']
+})
+export class ShowInfoSnackbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
