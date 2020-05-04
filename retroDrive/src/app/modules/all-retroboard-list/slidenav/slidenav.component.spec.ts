72a754b8996f42983e44db375610e44ca00b4d34
Adrian Nowakowski <anowakowski@mailc.net>
2020-05-04 10:28:25 +0200

    added slidenav comp

---
+++ retroDrive/src/app/modules/all-retroboard-list/slidenav/slidenav.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { SlidenavComponent } from './slidenav.component';
+
+describe('SlidenavComponent', () => {
+  let component: SlidenavComponent;
+  let fixture: ComponentFixture<SlidenavComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SlidenavComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SlidenavComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
