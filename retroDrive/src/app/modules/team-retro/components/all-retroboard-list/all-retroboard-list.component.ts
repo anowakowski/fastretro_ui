e0e67d967162e2bc5050a99cc187875307b95985
Adrian Nowakowski <anowakowski@mailc.net>
2020-05-04 11:28:22 +0200

    added functionality for all boards list

--- retroDrive/src/app/modules/team-retro/components/all-retroboard-list/all-retroboard-list.component.ts
+++ retroDrive/src/app/modules/team-retro/components/all-retroboard-list/all-retroboard-list.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartType, ChartOptions } from 'chart.js';
+import { FirestoreRetroBoardService } from '../../services/firestore-retro-board.service';
+import { RetroBoard } from 'src/app/models/retroBoard';
+import { RetroBoardToSave } from 'src/app/models/retroBoardToSave';
+import { User } from 'src/app/models/user';
+import { UserWorkspace } from 'src/app/models/userWorkspace';
+import { Workspace } from 'src/app/models/workspace';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { RetroBoardCardActions } from 'src/app/models/retroBoardCardActions';
+import { SingleDataSet, Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-all-retroboard-list',
@@ -7,9 +18,129 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllRetroboardListComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private firestoreRBServices: FirestoreRetroBoardService,
+    private authService: AuthService,
+    private localStorageService: LocalStorageService) { }
+
+  retroBoards: Array<RetroBoard> = new Array<RetroBoard>();
+
+  currentUser: User;
+  userWorkspace: UserWorkspace;
+  currentWorkspace: Workspace;
+
+  public pieChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  public pieChartLabels: Label[] = [['Negative Actions'], ['Positive Actions'], ''];
+  public pieChartData: SingleDataSet;
+  public pieChartType: ChartType = 'pie';
+  public pieChartLegend = false;
+  public pieChartPlugins = [];
 
   ngOnInit() {
+    this.currentUser = this.localStorageService.getItem('currentUser');
+
+    if (this.currentUser === undefined) {
+      this.authService.signOut();
+    } else {
+      if (!this.currentUser.isNewUser) {
+        this.userWorkspace = this.localStorageService.getItem('userWorkspace');
+        this.currentWorkspace = this.userWorkspace.workspaces.find(uw => uw.isCurrent);
+
+        this.prepreRetroBoardForCurrentWorkspace();
+      }
+    }
   }
 
+  private prepreRetroBoardForCurrentWorkspace() {
+    this.firestoreRBServices.retroBoardFilteredByWorkspaceIdSnapshotChanges(this.currentWorkspace.id).subscribe(retroBoardsSnapshot => {
+      this.retroBoards = new Array<RetroBoard>();
+      const finishedRetroBoards = new Array<RetroBoardToSave>();
+      const openRetroBoards = new Array<RetroBoardToSave>();
+
+      retroBoardsSnapshot.forEach(retroBoardSnapshot => {
+
+        const retroBoardData = retroBoardSnapshot.payload.doc.data() as RetroBoardToSave;
+        retroBoardData.id = retroBoardSnapshot.payload.doc.id as string;
+
+        retroBoardData.team.get().then(teamSnapshot => {
+          const team = teamSnapshot.data();
+          retroBoardData.team = team;
+
+          if (retroBoardData.isStarted) {
+            if (retroBoardData.isFinished) {
+              finishedRetroBoards.push(retroBoardData);
+            } else {
+              openRetroBoards.push(retroBoardData);
+            }
+
+            finishedRetroBoards.sort((a, b) => {
+              // tslint:disable-next-line:no-angle-bracket-type-assertion
+              return <any> new Date(b.creationDate) - <any> new Date(a.creationDate);
+            });
+
+            openRetroBoards.sort((a, b) => {
+              // tslint:disable-next-line:no-angle-bracket-type-assertion
+              return <any> new Date(b.creationDate) - <any> new Date(a.creationDate);
+            });
+
+            this.addToRetroBoards(finishedRetroBoards);
+            this.addToRetroBoards(openRetroBoards);
+
+            this.retroBoards.sort((a, b) => {
+              // tslint:disable-next-line:no-angle-bracket-type-assertion
+              return <any> a.isFinished - <any> b.isFinished;
+            });
+          }
+        });
+      });
+    });
+  }
+
+  private addToRetroBoards(retroBoards: RetroBoardToSave[]) {
+
+    retroBoards.forEach(retroBoard => {
+      if (retroBoard.isFinished) {
+        this.prepareActionForFinishedRetroBoardCards(retroBoard as RetroBoard);
+      } else {
+        this.retroBoards.push(retroBoard as RetroBoard);
+      }
+    });
+  }
+
+  private prepareActionForFinishedRetroBoardCards(finishedRetroBoard: RetroBoard) {
+    finishedRetroBoard.actions = new Array<RetroBoardCardActions>();
+
+    this.firestoreRBServices.retroBoardCardActionsFilteredByRetroBoardId(finishedRetroBoard.id).then(retroBoardCardActionsSnapshot => {
+      if (retroBoardCardActionsSnapshot.docs.length > 0) {
+
+        retroBoardCardActionsSnapshot.docs.forEach(retroBoardCardSnapshot => {
+          const dataRetroBoardCardAction = retroBoardCardSnapshot.data() as RetroBoardCardActions;
+          dataRetroBoardCardAction.text = retroBoardCardSnapshot.id as string;
+          finishedRetroBoard.actions.push(dataRetroBoardCardAction);
+        });
+      }
+      this.retroBoards.push(finishedRetroBoard);
+      this.prepareChartForFinishedRetroBoardActions(finishedRetroBoard.actions);
+    });
+  }
+
+  private prepareChartForFinishedRetroBoardActions(finiszedRetroBoardActions: RetroBoardCardActions[]) {
+    const wentWellActions = finiszedRetroBoardActions.filter(act => act.isWentWell);
+    const toImproveActions = finiszedRetroBoardActions.filter(act => !act.isWentWell);
+
+    const wentWellActionsCount = this.prepareCorrectValueForChartPieData(wentWellActions.length);
+    const toImproveActionsCount = this.prepareCorrectValueForChartPieData(toImproveActions.length);
+
+    this.pieChartData = [toImproveActionsCount, wentWellActionsCount, 0];
+  }
+
+  private prepareCorrectValueForChartPieData(value: number) {
+    if (!value) {
+      return 0;
+    }
+
+    return value;
+  }
 }
