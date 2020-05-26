24b067e6934be7b22e6e9c89a52c171fd31560cd
Adrian Nowakowski <anowakowski@mailc.net>
2020-05-26 10:48:25 +0200

    added display name

--- retroDrive/src/app/models/CurrentUserInRetroBoardDataToDisplay.ts
+++ retroDrive/src/app/models/CurrentUserInRetroBoardDataToDisplay.ts
@@ -2,4 +2,5 @@ export class CurrentUserInRetroBoardDataToDisplay {
     public firebaseUserDocId: string;
     public dateOfExistingCheck: string;
     public chosenAvatarUrl: string;
+    public displayName: string;
 }
