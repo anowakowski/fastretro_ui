51ae2e1fff49fa387fd8d574bc7d67ecd6fc1d7d
Adrian Nowakowski <anowakowski@mailc.net>
2020-06-02 11:13:40 +0200

    next part of workspace functionality

--- retroDrive/src/app/modules/team-retro/components/join-to-existing-workspace-dialog/join-to-existing-workspace-dialog.component.ts
+++ retroDrive/src/app/modules/team-retro/components/join-to-existing-workspace-dialog/join-to-existing-workspace-dialog.component.ts
@@ -3,9 +3,11 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Team } from 'src/app/models/team';
 import { FirestoreRetroBoardService } from '../../services/firestore-retro-board.service';
 import { Workspace } from 'src/app/models/workspace';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { UserTeamsToSave } from 'src/app/models/userTeamsToSave';
 import { UserWorkspaceToSave } from 'src/app/models/userWorkspacesToSave';
+import { User } from 'src/app/models/user';
+import { UserWorkspaceDataToSave } from 'src/app/models/userWorkspaceDataToSave';
 
 @Component({
   selector: 'app-join-to-existing-workspace-dialog',
@@ -15,7 +17,9 @@ import { UserWorkspaceToSave } from 'src/app/models/userWorkspacesToSave';
 export class JoinToExistingWorkspaceDialogComponent implements OnInit {
 
   joinToExisitngWorkspaceForm: FormGroup;
-  existingWorkspaceNameFormControl = new FormControl('');
+  existingWorkspaceNameFormControl = new FormControl('', Validators.required);
+
+  workspaceNotExist = false;
 
   constructor(
     public dialogRef: MatDialogRef<JoinToExistingWorkspaceDialogComponent>,
@@ -28,10 +32,35 @@ export class JoinToExistingWorkspaceDialogComponent implements OnInit {
     this.createActionForRetroBoardForm();
   }
 
-
   joinToExisitngWorkspace() {
-    const chosenTeamId = this.joinToExisitngWorkspaceForm.value.existingTeamIdFormControl;
+    if (this.joinToExisitngWorkspaceForm.valid) {
+      const workspaceName = this.joinToExisitngWorkspaceForm.value.existingWorkspaceNameFormControl;
+
+      this.firestoreService.findWorkspacesByName(workspaceName).then(workspaceSnapshot => {
+        if (workspaceSnapshot.docs.length === 0) {
+          // tslint:disable-next-line:object-literal-key-quotes
+          this.existingWorkspaceNameFormControl.setErrors({'notexists': true});
+        } else {
+          const findedWorkspace = workspaceSnapshot.docs[0];
+          const workspaceId = findedWorkspace.id;
+          this.createUserWorkspaces(this.data.currentUser, workspaceId);
+        }
+      });
+    }
   }
+
+  private createUserWorkspaces(findedUsr: User, workspaceId: string) {
+    const workspacesToAddToUserWorkspace: UserWorkspaceDataToSave = {
+      workspace: this.firestoreService.addWorkspaceAsRef(workspaceId),
+      isCurrent: true
+    };
+    const userWorkspace: UserWorkspaceToSave = {
+      userId: findedUsr.uid,
+      workspaces: [workspacesToAddToUserWorkspace]
+    };
+    this.firestoreService.addNewUserWorkspace(userWorkspace);
+  }
+
 
   onNoClick(): void {
     this.dialogRef.close();
