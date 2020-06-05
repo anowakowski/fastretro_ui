784274f7fc689f71288d387cbac990522b98dca1
Adrian Nowakowski <anowakowski@mailc.net>
2020-06-05 12:49:29 +0200

    first part of retro board opt with dialog

---
+++ retroDrive/src/app/modules/team-retro-process-in-progress/components/team-retro-in-progress-retro-board-options-dialog/team-retro-in-progress-retro-board-options-dialog-component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CurrentUsersInRetroBoard } from 'src/app/models/currentUsersInRetroBoard';
+import { CurrentUserInRetroBoardDataToDisplay } from 'src/app/models/CurrentUserInRetroBoardDataToDisplay';
+
+@Component({
+  selector: 'app-team-retro-in-progress-retro-board-options-dialog-component',
+  templateUrl: './team-retro-in-progress-retro-board-options-dialog-component.html',
+  styleUrls: ['./team-retro-in-progress-retro-board-options-dialog-component.css']
+})
+
+export class TeamRetroInProgressRetroBoardOptionsDialogComponent implements OnInit {
+
+  constructor(
+    public dialogRef: MatDialogRef<TeamRetroInProgressRetroBoardOptionsDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public dataCurrentUsersInRetroBoard: any) { }
+
+  ngOnInit() {
+  }
+
+  closeClick(): void {
+    this.dialogRef.close();
+  }
+}
