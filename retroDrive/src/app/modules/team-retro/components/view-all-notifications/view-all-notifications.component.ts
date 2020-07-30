b57cecb70c31d818df00523ff8eb6b94aba05a78
Adrian Nowakowski <anowakowski@mailc.net>
2020-07-30 10:10:00 +0200

    next part of notification style

--- retroDrive/src/app/modules/team-retro/components/view-all-notifications/view-all-notifications.component.ts
+++ retroDrive/src/app/modules/team-retro/components/view-all-notifications/view-all-notifications.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { CurrentUserApiService } from 'src/app/services/current-user-api.service';
+import { MatDialog } from '@angular/material/dialog';
+import { UserNotificationWorkspaceWithRequiredAccess } from 'src/app/models/userNotificationWorkspaceWithRequiredAccess';
+import { User } from 'src/app/models/user';
+import { UserWorkspace } from 'src/app/models/userWorkspace';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-view-all-notifications',
@@ -7,9 +14,93 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewAllNotificationsComponent implements OnInit {
 
-  constructor() { }
+  currentUser: User;
+  public userWorkspace: UserWorkspace;
+  public currentUserWorkspaceName: string;
+  public currentUserNotifications = new Array<UserNotificationWorkspaceWithRequiredAccess>();
+
+  constructor(
+    public auth: AuthService,
+    private localStorageService: LocalStorageService,
+    private currentUserInRetroBoardApiService: CurrentUserApiService,
+    public dialog: MatDialog) { }
 
   ngOnInit() {
+    this.currentUser = this.localStorageService.getItem('currentUser');
+
+    if (this.currentUser === undefined) {
+      this.auth.signOut();
+    } else {
+      if (!this.currentUser.isNewUser) {
+        this.userWorkspace = this.localStorageService.getItem('userWorkspace');
+        const currentWorkspace = this.userWorkspace.workspaces.find(uw => uw.isCurrent).workspace;
+        this.currentUserWorkspaceName = currentWorkspace.name;
+      }
+    }
+
+    this.getUserNotification();
   }
 
+  getUserNotification() {
+    if (this.currentUserInRetroBoardApiService.isTokenExpired()) {
+      this.currentUserInRetroBoardApiService.regeneraTokenPromise().then(refreshedTokenResponse => {
+        this.currentUserInRetroBoardApiService.setRegeneratedToken(refreshedTokenResponse);
+        this.getUserNotyficationFromApi();
+      });
+    } else {
+      this.getUserNotyficationFromApi();
+    }
+  }
+
+  isAceptedByOwnerAndIsApproved(userNotification: UserNotificationWorkspaceWithRequiredAccess): boolean {
+    const isAccpeted =
+      userNotification.userWaitingToApproveWorkspaceJoin.isApprovalByCreator &&
+      userNotification.userWaitingToApproveWorkspaceJoin.requestIsApprove;
+
+    return isAccpeted;
+  }
+
+  private getUserNotyficationFromApi() {
+    this.currentUserInRetroBoardApiService.getUserNotification(this.currentUser.uid)
+      .then(response => {
+        if (response !== undefined && response !== null) {
+          this.prepareUsrNotification(response);
+        }
+      })
+      .catch(error => {
+        const err = error;
+      });
+  }
+
+  private prepareUsrNotification(response: any) {
+    this.currentUserNotifications = new Array<UserNotificationWorkspaceWithRequiredAccess>();
+    if (response.userNotificationWorkspaceWithRequiredAccesses !== undefined &&
+        response.userNotificationWorkspaceWithRequiredAccesses !== null) {
+          if (response.userNotificationWorkspaceWithRequiredAccesses.length > 0) {
+            const userNotificationWorkspaceWithRequiredAccesses = response.userNotificationWorkspaceWithRequiredAccesses as any[];
+            userNotificationWorkspaceWithRequiredAccesses.forEach(userNotificationWorkspaceWithRequiredAccess => {
+              this.currentUserNotifications.push(userNotificationWorkspaceWithRequiredAccess);
+            });
+          }
+    }
+    if (response.userNotificationWorkspaceWithRequiredAccessResponses !== undefined &&
+        response.userNotificationWorkspaceWithRequiredAccessResponses !== null) {
+      if (response.userNotificationWorkspaceWithRequiredAccessResponses.length > 0) {
+        const userNotificationWorkspaceWithRequiredAccessResponses = response.userNotificationWorkspaceWithRequiredAccessResponses as any[];
+        userNotificationWorkspaceWithRequiredAccessResponses.forEach(userNotificationWorkspaceWithRequiredAccessResponse => {
+          this.currentUserNotifications.push(userNotificationWorkspaceWithRequiredAccessResponse);
+        });
+      }
+    }
+    this.sortCurrentUserNoitficationByIsReadByAsc();
+  }
+
+  sortCurrentUserNoitficationByIsReadByAsc() {
+    this.currentUserNotifications.sort((leftSide, rightSide): number => {
+      if (leftSide.userNotification.isRead < rightSide.userNotification.isRead) { return -1; }
+      if (leftSide.userNotification.isRead > rightSide.userNotification.isRead) { return 1; }
+
+      return 0;
+    });
+  }
 }
