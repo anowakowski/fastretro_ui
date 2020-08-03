f0e08a74d71c707b897fdb6a235aa9bc612adb88
Adrian Nowakowski <anowakowski@mailc.net>
2020-08-03 16:10:19 +0200

    more hide

--- retroDrive/src/app/services/local-storage.service.ts
+++ retroDrive/src/app/services/local-storage.service.ts
@@ -13,10 +13,11 @@ const TOKEN = 'Sa@LRm*Dc6mmKkyiu';
 export class LocalStorageService {
 
   constructor() {
-   this.secKey = environment.x_data;
+   this.x_da = environment.x_data;
   }
 
-  private secKey;
+  // tslint:disable-next-line:variable-name
+  private x_da: string;
   public currentUserKey  = CURRENT_USER;
   public currentWorkspaceKey = CURRENT_WORKSPACE;
   public userWorkspaceKey = USER_WORKSPACE;
@@ -32,13 +33,13 @@ export class LocalStorageService {
 
   setEncryptedItem(key: string, newItem: any) {
     const newItemAsJSON = JSON.stringify(newItem);
-    const encryptedData = CryptoJS.AES.encrypt(newItemAsJSON, this.secKey);
+    const encryptedData = CryptoJS.AES.encrypt(newItemAsJSON, this.x_da);
     localStorage.setItem(key, encryptedData.toString());
   }
 
   getDecryptedItem(key: string): any {
     const encryptedItem = localStorage.getItem(key);
-    const decryptedItem = CryptoJS.AES.decrypt(encryptedItem, this.secKey);
+    const decryptedItem = CryptoJS.AES.decrypt(encryptedItem, this.x_da);
     const itemJson = decryptedItem.toString(CryptoJS.enc.Utf8);
     const parsedItem = JSON.parse(itemJson);
     return parsedItem;
