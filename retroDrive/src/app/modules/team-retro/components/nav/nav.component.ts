357a8a24d99e44ab1923fa7cdd0cc776f71f729f
Adrian Nowakowski <anowakowski@mailc.net>
2020-10-01 12:50:18 +0200

    new image for user settings

--- retroDrive/src/app/modules/team-retro/components/nav/nav.component.ts
+++ retroDrive/src/app/modules/team-retro/components/nav/nav.component.ts
@@ -124,7 +124,7 @@ export class NavComponent implements OnInit, OnDestroy {
 
   private openUserSettingsDialogComponentDialog() {
     const dialogRef = this.dialog.open(UserSettingsDialogComponent, {
-      width: '1000px',
+      width: '1600px',
       data: {
         currentUser: this.currentUser
       }
