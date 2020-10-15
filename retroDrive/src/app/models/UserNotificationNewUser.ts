cb2bd4cd0b347fdf3fda69308dc04572437d1813
Adrian Nowakowski <anowakowski@mailc.net>
2020-10-15 13:12:17 +0200

    added usrNotification

---
+++ retroDrive/src/app/models/UserNotificationNewUser.ts
@@ -0,0 +1,6 @@
+export class UserNotificationNewUser {
+    public userFirebaseDocId: string;
+    public email: string;
+    public displayName: string;
+    public userNotificationFirebaseDocId: string;
+}
