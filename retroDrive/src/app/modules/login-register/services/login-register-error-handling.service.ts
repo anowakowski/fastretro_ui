0f50eb148c86e14b89dea2a69366ad6ffe15dc03
Adrian Nowakowski <anowakowski@mailc.net>
2020-11-19 14:57:29 +0100

    added next code

--- retroDrive/src/app/modules/login-register/services/login-register-error-handling.service.ts
+++ retroDrive/src/app/modules/login-register/services/login-register-error-handling.service.ts
@@ -13,15 +13,21 @@ export class LoginRegisterErrorHandlingService {
   private readonly internalErrorCode = 'auth/internal-error';
   private readonly invalidCredentialCode = 'auth/invalid-credential';
   private readonly invalidEmailCode = 'auth/invalid-email';
+
   private readonly invalidPasswordCode = 'auth/invalid-password';
   private readonly wrongPasswordCode = 'auth/wrong-password';
   private readonly weakPasswordCode = 'auth/weak-password';
+
+  private readonly googleCancelledPopupRequestCode = 'auth/cancelled-popup-request';
 
   private readonly messageInvalidEmail = 'The email address is not valid';
   private readonly messageUserNotFound = 'User with given email not exist';
   private readonly messageUserIsCurrentlyInUse = 'User with given email is currently exist';
   private readonly messageInvalidUserOrPass = 'Invalid user or password';
   private readonly messagePasswordIsWeak = 'Password should be at least 6 characters';
+
+  // tslint:disable-next-line:max-line-length
+  private readonly messageGoogleCancelledPopupRequest = 'This operation has been cancelled due to another conflicting popup being opened. Please refresh page and try again';
 
   private readonly messageSystemError = 'System error.';
 
@@ -68,6 +74,10 @@ export class LoginRegisterErrorHandlingService {
       }
       case this.weakPasswordCode: {
         messeage = this.messagePasswordIsWeak;
+        break;
+      }
+      case this.googleCancelledPopupRequestCode: {
+        messeage = this.messageGoogleCancelledPopupRequest;
         break;
       }
       default: {
