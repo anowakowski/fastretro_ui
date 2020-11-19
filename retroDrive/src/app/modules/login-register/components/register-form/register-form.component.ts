48e99a3d8ae23c814f9f49a51231cffe56659a1e
Adrian Nowakowski <anowakowski@mailc.net>
2020-11-19 14:41:25 +0100

    change min length to 6 next part of rules

--- retroDrive/src/app/modules/login-register/components/register-form/register-form.component.ts
+++ retroDrive/src/app/modules/login-register/components/register-form/register-form.component.ts
@@ -19,7 +19,7 @@ import { LoginRegisterErrorHandlingService } from '../../services/login-register
 export class RegisterFormComponent implements OnInit {
   addNewEmailPassRegisterForm: FormGroup;
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
-  passFormControl = new FormControl('', [Validators.required, Validators.minLength(8)]);
+  passFormControl = new FormControl('', [Validators.required, Validators.minLength(6)]);
 
   shouldHideChoseLoginOptionForm = false;
 
